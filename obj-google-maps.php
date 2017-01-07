b0d1b21f372dc5b85f5e71b35a50789709326a15
Wesley Cole <wes@cgd.io>
2017-01-07 13:06:31 -0500

    first commit

---
+++ obj-google-maps.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: Objectiv Google Maps
+Plugin URI:  http://objectiv.co
+Description: Create searchable Google maps
+Version:     1.0
+Author:      Objectiv
+Author URI:  http://objectiv.co
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: obj-google-maps
+Domain Path: /languages
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
