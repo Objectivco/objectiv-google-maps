e921494aac08e72d7c8777dc1028f2210a625bd8
Wesley Cole <wes@cgd.io>
2017-01-07 13:37:18 -0500

    adding check for shortcode function

---
+++ includes/obj-functions.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Check to see if page has a shortcode
+ * @param  string  $shortcode
+ * @return boolean
+ */
+function obj_has_shortcode( $shortcode = '' ) {
+
+    global $post;
+    $post_obj = get_post( $post->ID );
+    $found = false;
+
+    if ( ! $shortcode )
+        return $found;
+
+    if ( stripos( $post_obj->post_content, '[' . $shortcode ) !== false )
+        $found = true;
+
+    return $found;
+
+}
