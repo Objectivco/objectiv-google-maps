2a9eeddb37896a1343efb345a79ade7545028eb4
Wesley Cole <wes@cgd.io>
2017-01-08 17:22:11 -0500

    getting center from google places

--- assets/js/admin/build/main.js
+++ assets/js/admin/build/main.js
@@ -10,57 +10,11 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 _googleMaps2.default.KEY = data.api_key;
 _googleMaps2.default.LIBRARIES = ['places'];
 
-function fillInAddress() {
-    // Get the place details from the autocomplete object.
-    var place = autocomplete.getPlace();
-
-    console.log(place);
-
-    for (var component in componentForm) {
-        document.getElementById(component).value = '';
-        document.getElementById(component).disabled = false;
-    }
-
-    // Get each component of the address from the place details
-    // and fill the corresponding field on the form.
-    for (var i = 0; i < place.address_components.length; i++) {
-        var addressType = place.address_components[i].types[0];
-        if (componentForm[addressType]) {
-            var val = place.address_components[i][componentForm[addressType]];
-            document.getElementById(addressType).value = val;
-        }
-    }
-}
-
-// Bias the autocomplete object to the user's geographical location,
-// as supplied by the browser's 'navigator.geolocation' object.
-function geolocate() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var geolocation = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-            var circle = new google.maps.Circle({
-                center: geolocation,
-                radius: position.coords.accuracy
-            });
-            autocomplete.setBounds(circle.getBounds());
-        });
-    }
-}
-
 _googleMaps2.default.load(function (google) {
     var autocomplete;
 
-    document.getElementById('autocomplete').onfocus = function () {
-        geolocate();
-    };
-
     autocomplete = new google.maps.places.Autocomplete(
     /** @type {!HTMLInputElement} */document.getElementById('autocomplete'), { types: ['geocode'] });
-
-    autocomplete.addListener('place_changed', fillInAddress);
 });
 
 },{"google-maps":2}],2:[function(require,module,exports){
