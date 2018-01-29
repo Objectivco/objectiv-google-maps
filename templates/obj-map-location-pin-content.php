e12fbe6b0da0a6222a2e10927c1b5358e6d73d0a
Matthew Sigley <matthew@electricquilt.com>
2018-01-29 14:36:35 -0500

    Added apt and ste numbers to location pin content.

--- templates/obj-map-location-pin-content.php
+++ templates/obj-map-location-pin-content.php
@@ -1,7 +1,7 @@
 <div class="obj-google-maps-infowindow" itemscope itemtype="http://schema.org/LocalBusiness">
   <p itemprop="name"><strong><?php echo $obj_location_post_title; ?></strong></p>
   <p itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
-    <span class="street-address" itemprop="streetAddress"><?php echo "$obj_location_street_number $obj_location_route"; ?></span><br />
+    <span class="street-address" itemprop="streetAddress"><?php echo "$obj_location_street_number $obj_location_route"; ?><?php echo !empty( $obj_location_subpremise ) ? " #$obj_location_subpremise" : ''; ?></span><br />
     <span class="city-name" itemprop="addressLocality"><?php echo $obj_location_locality; ?></span
       >, <span class="state-name" itemprop="addressRegion"><?php echo $obj_location_administrative_area_level_1; ?></span
       > <span class="country-name" itemprop="addressCountry"><?php echo $obj_location_country; ?></span
